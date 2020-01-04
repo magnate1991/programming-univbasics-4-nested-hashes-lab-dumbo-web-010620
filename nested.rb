d42201a5e14429273715759441130791a5bf51e3
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-04 13:18:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -131,7 +131,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:adding_to_dennis]
-programmer_hash[:adding_to_dennis] = "Young"
-puts programmer_hash
+ programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 programmer_hash
 end
